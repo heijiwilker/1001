9f290c004e3c21c9e631b2258896bf8ca18d1be0
Wilker Heiji <heijiwilker@hotmail.com>
2024-02-15 16:22:53 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  Leia 2 valores inteiros e armazene-os nas variáveis A e B. 
+             *  Efetue a soma de A e B atribuindo o seu resultado na variável X. 
+             *  Imprima X conforme exemplo apresentado abaixo. 
+             *  Não apresente mensagem alguma além daquilo que está sendo 
+             *  especificado e não esqueça de imprimir o fim de linha após o resultado, 
+             *  caso contrário, você receberá "Presentation Error".
+             * 
+             *  Entrada
+             *  A entrada contém 2 valores inteiros.
+             *  
+             *  Saída
+             *  Imprima a mensagem "X = " (letra X maiúscula) seguido pelo valor da 
+             *  variável X e pelo final de linha. Cuide para que tenha um espaço 
+             *  antes e depois do sinal de igualdade, conforme o exemplo abaixo.
+             */
+
+            int A = Convert.ToInt32(Console.ReadLine());
+            int B = Convert.ToInt32(Console.ReadLine());
+
+            int X = A + B;
+
+            Console.WriteLine($"X = {X}");
+            Console.ReadKey();
+
+        }
+    }
+}
